80d95b18f3dd4ac2b350341c470470d5133dbe09
Raed438 <80750216+Raed438@users.noreply.github.com>
2021-03-16 13:23:30 +0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,93 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+. . . . . . f f f f . . . . . . 
+. . . . . f 1 1 1 1 f . . . . . 
+. . . . f f 1 8 8 1 f f . . . . 
+. . . f f f 1 1 1 8 f f f . . . 
+. . f f f 8 1 8 8 1 8 f f f . . 
+. . f f 8 8 1 1 1 1 8 8 8 f . . 
+. . f 8 8 f f f f f f 8 8 f . . 
+. . f f f f e e e e f f f f . . 
+. . f e f b f 4 4 f b f e f . . 
+. . f e 4 1 f d d f 1 4 e f . . 
+. . f 4 4 d d d d d d 4 4 f . . 
+. . . f 4 4 4 4 4 4 4 4 f . . . 
+. . 8 8 f 8 8 8 8 8 8 f 8 8 . . 
+. . 8 8 f 8 8 8 8 8 8 f 8 8 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image2":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f 1 5 5 1 f f . . . . 
+. . . f f f 1 1 1 1 f f f . . . 
+. . f f f 5 5 1 1 5 5 f f f . . 
+. . f 5 5 5 5 1 1 5 5 5 5 f . . 
+. . f 5 5 f f f f f f 5 5 f . . 
+. . f f f f e e e e f f f f . . 
+. . f e f b f 4 4 f b f e f . . 
+. . f e 4 1 f d d f 1 4 e f . . 
+. . f 4 4 d d d d d d 4 4 f . . 
+. . . f 4 4 4 4 4 4 4 4 f . . . 
+. . 5 5 f 5 5 5 5 5 5 f 5 5 . . 
+. . 5 5 f 5 5 5 5 5 5 f 5 5 . . 
+. . 4 4 f 4 4 2 2 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image3":return img`
+. . . . . . f f f f . . . . . . 
+. . . . . f 1 1 1 1 f . . . . . 
+. . . . f f 1 8 8 1 f f . . . . 
+. . . f f f 1 1 1 8 f f f . . . 
+. . f f f 8 1 8 8 1 8 f f f . . 
+. . f f 8 8 1 1 1 1 8 8 8 f . . 
+. . f 8 8 f f f f f f 8 8 f . . 
+. . f f f f e e e e f f f f . . 
+. . f e f b f 4 4 f b f e f . . 
+. . f e 4 1 f d d f 1 4 e f . . 
+. . f 4 4 d d d d d d 4 4 f . . 
+. . . f 4 4 4 4 4 4 4 4 f . . . 
+. . 8 8 f 8 8 8 8 8 8 f 8 8 . . 
+. . 8 8 f 8 8 8 8 8 8 f 8 8 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image4":return img`
+. . . . . . f f f f . . . . . . 
+. . . . . f 1 1 1 1 f . . . . . 
+. . . . f f 1 7 7 7 f f . . . . 
+. . . f f f 1 7 1 1 f f f . . . 
+. . f f f 7 1 7 7 1 7 f f f . . 
+. . f 7 7 7 1 1 1 1 7 7 7 f . . 
+. . f 7 7 f f f f f f 7 7 f . . 
+. . f f f f e e e e f f f f . . 
+. . f e f b f 4 4 f b f e f . . 
+. . f e 4 1 f d d f 1 4 e f . . 
+. . f e 4 d d d d d d 4 4 f . . 
+. . . f 4 4 4 4 4 4 4 4 f . . . 
+. . 7 7 f 7 7 7 7 7 7 f 7 7 . . 
+. . 7 7 f 7 7 7 7 7 7 f 7 7 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
